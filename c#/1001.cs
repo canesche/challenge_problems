327636820836a6a93e13dd4d92e559c0fe8b91e8
canesche <michael.canesche@gmail.com>
2019-10-08 14:01:14 -0300

    new problem

---
+++ c#/1001.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int a, b;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        
+        int c = a + b;
+
+        Console.WriteLine("X = {0}", c);
+
+    }
+
+}
---
+++ go/1001.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    
+    var a, b, c int;
+    
+    fmt.Scan(&a,&b);
+    
+    c = a + b;
+    
+    fmt.Printf("X = %d\n",c)
+}
