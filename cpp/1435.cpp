389b7e0f3a30ca815256919d8c9354374ec85553
canesche <michael.canesche@gmail.com>
2021-08-12 09:34:22 -0300

    update

--- cpp/1435.cpp
+++ cpp/1435.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, r;
     
     while (true) {
         cin >> n;
@@ -11,23 +11,31 @@ int main(){
         
         int v[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; ++j) {
-                v[i][j] = 0;
-            }
-        }
-
-        for (int c = 0; c < 1; c++){
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; ++j) {
-                    v[i][j] += 1;
-                }
-            }
-        }
+        if(n%2 == 0)
+            r = n/2;
+        else
+            r = n/2 + 1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; ++j) {
-                cout << "   " << v[i];
+                v[i][j] = 1;
+            }
+        }
+
+        for(int c = 1; c < r; c++){
+			for(int i = c; i < n-c; i++){
+				for(int j = c; j < n-c; j++){
+					v[i][j] = c+1;
+				}
+			}
+		}
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; ++j) {
+                if (j == 0)
+                    printf("%3d", v[i][j]);
+                else
+                    printf(" %3d", v[i][j]);
             }
             cout << endl;
         }
