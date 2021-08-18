5f660164d3cbddb758614ea04386bb377f56e341
canesche <michael.canesche@gmail.com>
2021-08-18 07:45:02 -0300

    update

--- cpp/1973.cpp
+++ cpp/1973.cpp
@@ -2,35 +2,32 @@
 using namespace std;
 
 int main(){
-    long int n;
+    long long int n;
     cin >> n;
-    long int v[n];
-	long int t[n];
+    long long int v[n];
+	long long int t[n];
 
-    long int i;
-
-    for(i = 0; i < n; ++i) {
+    for(int i = 0; i < n; ++i) {
         cin >> v[i];
 		t[i] = 0;
 	}
 
-	long int c = 0;
-	long int aux;
+	long long int c = 0;
+	long long int aux;
 	
-    i = 0;
+    int i = 0;
     while(true) {
         if(i < 0 || i >= n)
             break;
-        else {
-			aux = v[i];
-			t[i] = 1;
-			if (v[i] > 0) --v[i];
-			(aux % 2 == 0) ? --i : ++i;
-        }
+        
+		aux = v[i];
+		t[i] = 1;
+		if (v[i] > 0) --v[i];
+		(aux % 2 == 0) ? --i : ++i;
     }
 
-	long int sum = 0, sum2 = 0;
-	for(i = 0; i < n; i++){
+	long long int sum = 0, sum2 = 0;
+	for(int i = 0; i < n; i++){
 		sum += v[i];
 		sum2 += t[i];
 	}
